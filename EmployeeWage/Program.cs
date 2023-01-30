65a0c3c52a87e723e6efd1bda32ace8f74bcd0ba
ajeet1010 <ajeetpal101999@gmail.com>
2023-01-31 00:36:31 +0530

    [Ajeet] Employee Wage Problems

---
+++ EmployeeWage/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmployeeWage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-----  Employee Wage Problems  -----");
+        }
+    }
+}
