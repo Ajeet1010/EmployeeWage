81e62b09c10c4a41a8a0d0dd9affe127cb93253a
ajeet1010 <ajeetpal101999@gmail.com>
2023-02-10 01:09:55 +0530

    [Ajeet] Wage for employee working in multiple companies

--- EmployeeWage/Multiple.cs
+++ EmployeeWage/Multiple.cs
@@ -6,18 +6,15 @@ using System.Threading.Tasks;
 
 namespace EmployeeWage
 {
-    internal class Maxhrs
+    internal class Multiple
     {
         const int Is_Full_Time = 1;
         const int Is_Part_Time = 2;
-        const int Hourwage = 20;
-        const int Workingdays = 20;
-        const int maxhrs = 100;
-        public void Wage()
+        public void Companies(string company, int hourwage, int workingdays, int workinghrs)
         {
             int emphrs = 0, totalEmphrs = 0, day =1, empWage, totalEmpWage = 0;
             Random random = new Random();
-            while (day <= Workingdays && totalEmphrs <= maxhrs)
+            while (day <= workingdays && totalEmphrs <= workinghrs)
             {
                 int empAttendance = random.Next(0, 3); 
                 switch (empAttendance)
@@ -35,16 +32,14 @@ namespace EmployeeWage
                         Console.WriteLine("\nEmployee is Absent");
                         break;
                 }
-                empWage = emphrs * Hourwage;
+                empWage = emphrs * hourwage;
                 totalEmpWage = totalEmpWage + empWage;
-                Console.WriteLine("Employee Wage for day {0} is {1} and working hours is {2}", day, empWage, totalEmphrs);
+                //Console.WriteLine("Employee Wage for day {0} is {1} and working hours is {2}", day, empWage, totalEmphrs);
                 day++;
-                if (day < 21)
-                    totalEmphrs = totalEmphrs + emphrs;
-                else
-                    totalEmphrs = totalEmphrs;
+                totalEmphrs = totalEmphrs + emphrs;
             }            
-            Console.WriteLine("\nMonthly Wage for Employee for {0} days is {1} and total working hours in month is {2}", (day - 1), totalEmpWage, totalEmphrs);
+            Console.WriteLine("\nMonthly Wage for Employee working in {0} for {1} days and total working hours is {2} and salary will be {3}", company, (day - 1),
+                totalEmphrs, totalEmpWage);
         }
     }
 }
--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -7,9 +7,13 @@ namespace EmployeeWage
         static void Main(string[] args)
         {
             Console.WriteLine("-----  Employee Wage Problems  -----");
-            Console.WriteLine("Calculating the monthly salary of Employee\n");
-            Maxhrs monthly = new Maxhrs();
-            monthly.Wage();
+            Console.WriteLine("Calculating the monthly salary of Employee working in multiple companies\n");
+            Multiple monthly1 = new Multiple();
+            monthly1.Companies("HCL", 12, 10, 5);
+            Multiple monthly2 = new Multiple();
+            monthly2.Companies("Tata Motors", 25, 20, 30);
+            Multiple monthly3 = new Multiple();
+            monthly3.Companies("Wipro", 25, 30, 45);
         }
     }
 }
